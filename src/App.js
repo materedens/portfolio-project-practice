efa95ad14ca84e2863710a4a864042d70d35420e
materedens <denismatere@gmail.com>
2021-10-26 00:19:03 +0300

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 function App() {
   return (
+    <>
+    <Particles
+    className="particles-canvas"
+    params={{
+      Particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "star",
+          width: 6,
+          color: "#f9ab00"
+        }
+      }
+    }}
+     />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
